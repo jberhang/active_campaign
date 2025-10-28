b989fa3321c7b6aa184c5181029c9030bad34b48
Justin Berhang <justin@scratch.com>
2025-10-28 12:02:43 -0400

    add nil checks to prevent NoMethodError

--- lib/active_campaign/api/account_custom_field.rb
+++ lib/active_campaign/api/account_custom_field.rb
@@ -6,7 +6,8 @@ module ActiveCampaign
       end
 
       def find_account_custom_field_data(account_id, field_id)
-        response = show_account_custom_field_data(account_id)        
+        response = show_account_custom_field_data(account_id)
+        return nil if response.nil?
         found = response["customerAccountCustomFieldData"].select{|cfm| cfm["custom_field_id"] == field_id}
         found.first if found.any?
       end
@@ -21,12 +22,14 @@ module ActiveCampaign
 
       def find_account_custom_field_meta(field_label)
         response = get("/accountCustomFieldMeta")
+        return nil if response.nil?
         found = response["accountCustomFieldMeta"].select{|cfm| cfm["fieldLabel"] == field_label}
         found.first if found.any?
       end     
 
       def find_account_custom_field_text_value(account_id, field_label) 
         response = find_account_custom_field_meta(field_label)
+        return nil if response.nil?
         field_id = response["id"]
         field_data = find_account_custom_field_data(account_id, field_id)
         field_data["custom_field_text_value"] if field_data        
